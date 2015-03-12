3856ed2783d61cf312c034b7442b0d6a1995925d
Lucho <lucho.bonatti@devteam.com>
2015-03-12 17:03:29 -0300

    1

--- js/misc.js
+++ js/misc.js
@@ -126,10 +126,10 @@ function showMap(){
     })
 }
 
-document.addEventListener("deviceready", detectOs(), false);
-
-function detectOs() {
-    var deviceType = (navigator.userAgent.match(/iPad/i)) == "iPad" ? "iPad" : (navigator.userAgent.match(/iPhone/i)) == "iPhone" ? "iPhone" : (navigator.userAgent.match(/Android/i)) == "Android" ? "Android" : (navigator.userAgent.match(/BlackBerry/i)) == "BlackBerry" ? "BlackBerry" : "null";
-
-    deviceInfo = deviceType;
-}
+//document.addEventListener("deviceready", detectOs(), false);
+//
+//function detectOs() {
+//    var deviceType = (navigator.userAgent.match(/iPad/i)) == "iPad" ? "iPad" : (navigator.userAgent.match(/iPhone/i)) == "iPhone" ? "iPhone" : (navigator.userAgent.match(/Android/i)) == "Android" ? "Android" : (navigator.userAgent.match(/BlackBerry/i)) == "BlackBerry" ? "BlackBerry" : "null";
+//
+//    deviceInfo = deviceType;
+//}
