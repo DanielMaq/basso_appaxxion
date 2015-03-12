721f2e0bababb0355c3b8b96de31106e26dbb992
Lucho <lucho.bonatti@devteam.com>
2015-03-12 16:39:36 -0300

    1

--- js/misc.js
+++ js/misc.js
@@ -1,5 +1,5 @@
 var ScrrenOrientation;
-
+var deviceInfo;
 function rnd(min,max){
     p = max - min;
     aleat = Math.random() * p;
@@ -124,4 +124,12 @@ function showMap(){
             $(this).trigger('touchstart');
         }
     })
+}
+
+document.addEventListener("deviceready", detectOs(), false);
+
+function detectOs() {
+    var deviceType = (navigator.userAgent.match(/iPad/i)) == "iPad" ? "iPad" : (navigator.userAgent.match(/iPhone/i)) == "iPhone" ? "iPhone" : (navigator.userAgent.match(/Android/i)) == "Android" ? "Android" : (navigator.userAgent.match(/BlackBerry/i)) == "BlackBerry" ? "BlackBerry" : "null";
+
+    deviceInfo = deviceType;
 }
